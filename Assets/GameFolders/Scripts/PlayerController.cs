59dcbdde4a2150ff8d9e3fd110c2a7841a7d7219
AtaGn <ata_gn@hotmail.com>
2023-04-08 05:20:11 +0300

    PlayerCamMovement

---
+++ Assets/GameFolders/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [Header("References")]
+    public Rigidbody rb;
+    public Transform head;
+    public Camera camera;
+
+
+
+
+    [Header("Runtime")]
+    Vector3 newVelocity;
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+        //  Hide and lock the mouse cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update() {
+        // Horizontal rotation
+
+        float yRotation = transform.eulerAngles.y;
+        // Debug.Log("Current y rotation: " + yRotation);
+        yRotation += Input.GetAxis("Mouse X") * 2f;
+        // Debug.Log("Current y rotation: " + Vector3.up * Input.GetAxis("Mouse X") * 2f);
+        if ((yRotation >= 275) || (yRotation <= 90))
+        {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);   // Adjust the multiplier for different rotation speed
+        }
+
+    }
+
+
+    void LateUpdate() {
+        // Vertical rotation
+        Vector3 e = head.eulerAngles;
+        e.x -= Input.GetAxis("Mouse Y") * 2f;   //  Edit the multiplier to adjust the rotate speed
+        e.x = RestrictAngle(e.x, -85f, 85f);    //  This is clamped to 85 degrees. You may edit this.
+        head.eulerAngles = e;
+    }
+
+
+
+    //  A helper function
+    //  Clamp the vertical head rotation (prevent bending backwards)
+    public static float RestrictAngle(float angle, float angleMin, float angleMax) {
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+
+        if (angle > angleMax)
+            angle = angleMax;
+        if (angle < angleMin)
+            angle = angleMin;
+
+        return angle;
+    }
+}
